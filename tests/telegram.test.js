b81cee17d53eccf38e6cd35570294d5a4cf91b67
José Daniel  Quintero <josedanq100@gmail.com>
2020-06-06 20:17:14 -0400

    Tests

---
+++ tests/telegram.test.js
@@ -0,0 +1,13 @@
+const config = require('../config');
+const {telegramAPIKey, telegramChatID} = config.telegram;
+const TelegramBot = require('../telegram/telegram-bot');
+
+const telegramBot = new TelegramBot(telegramAPIKey, telegramChatID);
+
+// Test telegram bot connection
+test('Test getMe method of telegram bot', () => {
+  return telegramBot.getMe()
+      .then((data) => {
+        expect(data.status).toBe(200);
+      });
+}, 5000);
