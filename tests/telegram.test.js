2d0109325ec3ba2c39306c5116c1131c13d69b2f
José Daniel  Quintero <josedanq100@gmail.com>
2020-06-06 21:36:28 -0400

    Formatting tests

--- tests/telegram.test.js
+++ tests/telegram.test.js
@@ -5,8 +5,10 @@ const TelegramBot = require('../telegram/telegram-bot')
 const telegramBot = new TelegramBot(telegramAPIKey, telegramChatID)
 
 // Test telegram bot connection
-test('Test getMe method of telegram bot', () => {
-    return telegramBot.getMe().then((data) => {
-        expect(data.status).toBe(200)
-    })
-}, 5000)
+describe('Tests of telegram Bot', () => {
+    test('Test getMe method of telegram bot', () => {
+        return telegramBot.getMe().then((data) => {
+            expect(data.status).toBe(200)
+        })
+    }, 8000)
+})
