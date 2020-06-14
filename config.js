b3521b043b3970c65d23c950bef186fa3a3d5a78
José Daniel  Quintero <josedanq100@gmail.com>
2020-06-13 20:11:27 -0400

    Created default config vars

--- config.js
+++ config.js
@@ -7,15 +7,16 @@ const config = {
         telegramAPIKey: env.TELEGRAM_API_KEY,
         telegramChatID: env.TELEGRAM_CHAT_ID,
         webhookURL: env.TELEGRAM_WEBHOOK_URL,
-        telegramAPIURL: env.TELEGRAM_API_URL,
+        telegramAPIURL: env.TELEGRAM_API_URL || 'https://api.telegram.org/bot',
     },
     bitfinex: {
         bitfinexAPIKey: env.BITFINEX_API_KEY,
         bitfinexSecret: env.BITFINEX_API_SECRET,
-        bitfinexPublicURL: env.BITFINEX_PUBLIC_URL,
+        bitfinexPublicURL:
+            env.BITFINEX_PUBLIC_URL || 'wss://api-pub.bitfinex.com/ws/2',
     },
     server: {
-        port: env.SERVER_PORT,
+        port: env.SERVER_PORT || 3000,
         url: env.SERVER_URL,
     },
 }
