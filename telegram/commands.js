a8cee4f2868e4a46fe9a3ff71759ff6bb0aac11a
José Daniel  Quintero <josedanq100@gmail.com>
2020-07-15 20:55:13 -0400

    feat: the bot now can set commands using commands.js

---
+++ telegram/commands.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        command: 'start',
+        description: 'Connect to the api server',
+    },
+    {
+        command: 'subscribe',
+        description: 'Subscribe to candle updates',
+    },
+    {
+        command: 'routine',
+        description: `Send signals by intervals, using words "mins" or "hrs" to set it`,
+    },
+    {
+        command: 'screenshot',
+        description: `Send an screenshot from client app of BTC market on 3hrs candels`,
+    },
+    {
+        command: 'help',
+        description: 'Show the bot info',
+    },
+    {
+        command: 'stop',
+        description: 'Stop the bot server',
+    },
+]
